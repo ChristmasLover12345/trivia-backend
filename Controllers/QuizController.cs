1a4339895cce04f87aa2611fd03da9dff22e2467
ChristmasLover12345 <madnes0205@gmail.com>
2025-04-25 11:06:04 -0700

    updated cotrollers

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -2,21 +2,83 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using trivia_backend.Models;
 using trivia_backend.Services;
 
 namespace trivia_backend.Controllers
 {
-    public class QuizController
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : ControllerBase
     {
-        
-
-        private readonly QuizServices _quizServices;
+          private readonly QuizServices _quizServices;
         public QuizController(QuizServices quizServices)
         {
             _quizServices = quizServices;
         }
 
-        
+        [HttpGet("GetAllQuizzes")]
+        public async Task<IActionResult> GetAllQuizzes()
+        {
+            var quizzes = await _quizServices.GetAllQuizzes();
+
+            if (quizzes == null )
+            {
+                return NotFound("No quizzes found.");
+            }
+
+            return Ok(quizzes);
+        }
+
+        [HttpGet("GetQuizzesById/{id}")]
+        public async Task<IActionResult> GetQuizzesById(int id)
+        {
+            var quizzes = await _quizServices.GetQuizzesById(id);
+
+            if (quizzes == null )
+            {
+                return NotFound($"No quizzes found with ID {id}.");
+            }
+
+            return Ok(quizzes);
+        }
+
+        [HttpPost("CreateQuiz")]
+        public async Task<IActionResult> CreateQuiz([FromBody] QuizModel quiz)
+        {
+            if (quiz == null)
+            {
+                return BadRequest("Quiz data is null.");
+            }
+
+            var result = await _quizServices.CreateQuiz(quiz);
+
+            if (!result)
+            {
+                return StatusCode(500, "An error occurred while creating the quiz.");
+            }
+
+            return Ok(quiz);
+        }
+
+        [HttpPut("UpdateQuiz")]
+        public async Task<IActionResult> UpdateQuiz([FromBody] QuizModel quiz)
+        {
+            if (quiz == null)
+            {
+                return BadRequest("Quiz data is null.");
+            }
+
+            var result = await _quizServices.UpdateQuiz(quiz);
+
+            if (!result)
+            {
+                return StatusCode(500, "An error occurred while updating the quiz.");
+            }
+
+            return Ok(quiz);
+        }
 
     }
 }
